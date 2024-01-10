0320c62946b193eb168ef7661f95de90f764f92d
seyifash <fasugbeoluwaseyi@gmail.com>
2024-01-10 21:44:34 +0100

    can unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+def canUnlockAll(boxes):
+    if not boxes or not boxes[0]:
+        return False
+
+    num_boxes = len(boxes)
+    unlocked = [False] * num_boxes
+    unlocked[0] = True
+    keys = set(boxes[0])
+
+    while True:
+        prev_unlocked = sum(unlocked)
+
+        for i in range(num_boxes):
+            if unlocked[i]:
+                keys.update(boxes[i])
+
+        for key in keys:
+            if key < num_boxes and not unlocked[key]:
+                unlocked[key] = True
+
+        if sum(unlocked) == prev_unlocked:
+            break
+
+    return sum(unlocked) == num_boxes
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+The interview question, can unlock boxes.
+This is a method that checks if all the boxes contains all the keys to each box
