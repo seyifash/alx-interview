d662d4e906e6dc73e16850d9e8c682196cd3e3c0
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-14 14:07:06 +0100

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -11,33 +11,33 @@ if (!movieId) {
 
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, function(error, response, body){
-        if(error){
-        console.log("ERROR:", error);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('ERROR:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.log('Error fetching data:', response.statusCode);
+  }
+
+  const movieData = JSON.parse(body);
+  const charactersUrl = movieData.characters;
+
+  charactersUrl.forEach((characterUrl) => {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
         process.exit(1);
-        }
+      }
 
-        if (response.statusCode !== 200) {
-        console.log("Error fetching data:", response.statusCode);
-        }
+      if (response.statusCode !== 200) {
+        console.error('Failed to fetch character data, status code:', response.statusCode);
+        process.exit(1);
+      }
 
-        const movieData = JSON.parse(body);
-        const charactersUrl = movieData.characters;
-
-        charactersUrl.forEach((characterUrl) => {
-        request(characterUrl, function (error, response, body) {
-        if (error) {
-            console.error('Error:', error);
-            process.exit(1);
-    }
-
-    if (response.statusCode !== 200) {
-            console.error('Failed to fetch character data, status code:', response.statusCode);
-            process.exit(1);
-    }
-
-    const characterData = JSON.parse(body);
-    console.log(characterData.name);
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
     });
-    });
-});       
+  });
+});
