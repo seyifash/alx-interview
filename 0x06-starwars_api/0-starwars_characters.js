735bfd03e50df12310c37c1e34eaf108584c40f4
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-14 13:53:12 +0100

    star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movieId>');
+  process.exit(1);
+}
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, function(error, response, body){
+        if(error){
+        console.log("ERROR:", error);
+        process.exit(1);
+        }
+
+        if (response.statusCode !== 200) {
+        console.log("Error fetching data:", response.statusCode);
+        }
+
+        const movieData = JSON.parse(body);
+        const charactersUrl = movieData.characters;
+
+        charactersUrl.forEach((characterUrl) => {
+        request(characterUrl, function (error, response, body) {
+        if (error) {
+            console.error('Error:', error);
+            process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+            console.error('Failed to fetch character data, status code:', response.statusCode);
+            process.exit(1);
+    }
+
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    });
+    });
+});       
