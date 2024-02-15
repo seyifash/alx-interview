64fbbd5d7d5d2747ac5d153ff77f2e11aa4d7f38
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-15 13:16:34 +0100

    try fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,15 +6,15 @@ const movieId = process.argv[2];
 
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-function logchars (array, i) {
+function sendRequest (array, i) {
   if (i === array.length) {
     return;
   }
 
-  request(array[i], function (err, response1, body1) {
+  request(array[i], function (err, response1, body) {
     if (!err) {
-      console.log(JSON.parse(body1).name);
-      logchars(array, i + 1);
+      console.log(JSON.parse(body).name);
+      sendRequest(array, i + 1);
     }
   });
 }
@@ -23,7 +23,7 @@ request(url + movieId, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const cha = JSON.parse(body).characters;
-    logchars(cha, 0);
+    const url2 = JSON.parse(body).characters;
+    sendRequest(url2, 0);
   }
 });
