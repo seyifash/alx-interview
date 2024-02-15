abcaf90d8970e643e141b7ac40c23bd4cf877e79
seyifash <fasugbeoluwaseyi@gmail.com>
2024-02-15 12:58:49 +0100

    if it works

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,25 +4,25 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/`;
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log('ERROR:', error);
-  } else {
-    const charactersUrl = JSON.parse(body).characters;
-    if (charactersUrl.length === 0) {
-      return;
+function logchars (array, i){
+  if (i == array.length) {
+    return;
+  }
+
+  request.get(array[i], function (err, response1, body1) {
+    if (!err) {
+      console.log(JSON.parse(body1).name);
+      logchars(array, i + 1);
     }
+  });
+}
 
-    charactersUrl.forEach((characterUrl) => {
-      request(characterUrl, function (error, response, body) {
-        if (error) {
-          console.error('error:', error);
-        }
-        const characterData = JSON.parse(body);
-        console.log(characterData.name);
-      });
-    });
+request.get(url + movieId, function (error, response, body) {
+  if (!error) {
+    const cha = JSON.parse(body).characters;
+    logchars(cha, 0);
   }
 });
+  
