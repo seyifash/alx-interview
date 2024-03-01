a7e47e89f3e12d5c4f159acb519ead27a6c0fc25
Osei <williamsabodunrin@gmail.com>
2024-03-01 01:43:00 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
 
+def makeChange(coins, total):
+
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    index = 0
+    value = 0
+    count = 0
+    while index < len(coins):
+        value = value + coins[index]
+        if value > total:
+            if index < len(coins) - 1:
+                value = value - coins[index]
+                index = index + 1
+            else:
+                return -1
+        elif value < total:
+            count = count + 1
+        elif value == total:
+            count = count + 1
+            return count
